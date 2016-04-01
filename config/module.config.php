4da773f0013a024b147e68f1c43a71b588868104
Stefano Valle <s.valle@mvassociati.it>
2016-04-01 12:30:07 +0200

    First commit

---
+++ config/module.config.php
@@ -0,0 +1,15 @@
+<?php
+namespace Application;
+
+return [
+    
+    // assets
+    'asset_manager' => [
+        'resolver_configs' => [
+            'paths' => [
+                __DIR__ . '/../public',
+            ],
+        ],
+    ],
+
+];
